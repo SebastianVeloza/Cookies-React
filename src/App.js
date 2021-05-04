12fc2169a5334a2674b82463c645d56e410912dd
SebastianVeloza <fernando7829@hotmail.com>
2021-05-04 16:29:58 -0500

    pagina web de cookies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.handleTipo = this.handleTipo.bind(this);
+    this.handleReload = this.handleReload.bind(this);
+  }
+  handleTipo(e) {
+    cookies.set(`Tipo`, `${e.target.value}`, { path: "/" });
+  }
+  handleReload(e) {
+    e.preventDefault();
+    window.location.reload();
+  }
+  render() {
+    return (
+      <div className="P">
+        <div className="buttons">
+          <h1>Selecciona el tipo de letra.</h1>
+          <button className="b1" onClick={this.handleTipo} value="Courier New">
+            Courier New
+          </button>
+          <button
+            className="b2"
+            onClick={this.handleTipo}
+            value="Lucida Handwriting"
+          >
+            Lucida Handwriting
+          </button>
+          <button className="b3" onClick={this.handleReload}>
+            Actualizar
+          </button>
+          
+        </div>
+        <div className="text">
+        <p style={{ fontFamily: cookies.get(`Tipo`) }}>
+            JUAN SEBASTIAN FERNANDO VELOZA CERON
+          </p>
+          </div>
+      </div>
+    );
+  }
 }
-
-export default App;
